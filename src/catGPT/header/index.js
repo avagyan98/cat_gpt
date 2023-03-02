a3d2626529774c31568431a213ce2c597b5d2acc
yeghishe98 <yeghishe.avagyan@picsart.com>
2023-03-02 19:48:32 +0400

    cat gpt

---
+++ src/catGPT/header/index.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <div>
+            <h1> CatGPT </h1>
+            <p> What if ChatGPT were a cat? </p>
+        </div>
+    )
+}
+
+export default Header
